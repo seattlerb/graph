e9b940479def866830d37edc2e3d9e0a62b657cc
Ryan Davis <ryand@zenspider.com>
2009-03-27 16:51:35 -0800

    Added freebsd ports plugin [git-p4: depot-paths = "//src/graph/dev/": change = 4802]

---
+++ lib/freebsd_analyzer.rb
@@ -0,0 +1,19 @@
+class FreebsdAnalyzer < DepAnalyzer
+  def installed
+    # don't cache so it updates every delete
+    puts "scanning installed ports"
+    `pkg_info`.split(/\n/).map { |s| s.split.first }
+  end
+
+  def outdated
+    # don't cache so it updates every delete
+    puts "scanning outdated ports"
+    `pkg_version -vL=`.split(/\n/)[1..-1].map { |s| s.split.first }
+  end
+
+  def deps port
+    cache("#{port}.deps") {
+      `pkg_info -r #{port}`
+    }.grep(/Dependency:/).map { |s| s.split[1] }
+  end
+end
