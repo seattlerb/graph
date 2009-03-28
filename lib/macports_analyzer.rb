43f906ae1f42d40faf64f420da91f39a4bdf657c
Ryan Davis <ryand@zenspider.com>
2009-03-27 16:36:42 -0800

    Added macports and rubygems plugins [git-p4: depot-paths = "//src/graph/dev/": change = 4801]

---
+++ lib/macports_analyzer.rb
@@ -0,0 +1,19 @@
+class MacportsAnalyzer < DepAnalyzer
+  def installed
+    # don't cache so it updates every delete
+    puts "scanning installed ports"
+    `port installed`.split(/\n/)[1..-1].map { |s| s.split.first }
+  end
+
+  def outdated
+    # don't cache so it updates every delete
+    puts "scanning outdated ports"
+    `port outdated`.split(/\n/)[1..-1].map { |s| s.split.first }
+  end
+
+  def deps port
+    cache("#{port}.deps") {
+      `port deps #{port}`
+    }.scan(/^\t(\S+)$/).flatten
+  end
+end
---
+++ lib/rubygems_analyzer.rb
@@ -0,0 +1,20 @@
+class RubygemsAnalyzer < DepAnalyzer
+  def installed
+    require 'rubygems'
+    ENV['GEM_PATH'] = `gem env home`
+    Gem.clear_paths
+    # don't cache so it updates every delete
+    puts "scanning installed rubygems"
+    Gem.source_index.gems.values.map { |gem| gem.name }.sort
+  end
+
+  def outdated
+    # don't cache so it updates every delete
+    puts "scanning outdated rubygems"
+    Gem.source_index.outdated.sort
+  end
+
+  def deps port
+    Gem.source_index.find_name(port).first.dependencies.map { |dep| dep.name }
+  end
+end
