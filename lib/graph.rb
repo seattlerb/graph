df3280750a82ae1c3c5c1e98616f32239cf31ef4
Ryan Davis <ryand@zenspider.com>
2010-03-05 15:39:37 -0800

    + Added full rdoc for Graph [git-p4: depot-paths = "//src/graph/dev/": change = 5687]

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,25 +1,58 @@
 #!/usr/local/bin/ruby -w
 
+##
+# Graph is a type of hash that outputs in graphviz's dot format.
+
 class Graph < Hash
-  VERSION = '1.1.0'
+  VERSION = '1.1.0' # :nodoc:
+
+  ##
+  # A Hash of arrays of attributes for each node. Eg:
+  #
+  #   graph.attribs["a"] << "color = red"
+  #
+  # Will color node "a" red.
 
   attr_reader :attribs
-  attr_reader :prefix
+
+  ##
+  # An array of the order of traversal / definition of the nodes.
+  #
+  # You (generally) should leave this alone.
+
   attr_reader :order
+
+  ##
+  # An array of attributes to add to the front of the graph source. Eg:
+  #
+  #   graph.prefix << "ratio = 1.5"
+
+  attr_reader :prefix
+
+  ##
+  # A Hash of a Hashes of Arrays of attributes for an edge. Eg:
+  #
+  #   graph.edge["a"]["b"] << "color = blue"
+  #
+  # Will color the edge between a and b blue.
+
   attr_reader :edge
 
-  def []= key, val
+  def []= key, val # :nodoc:
     @order << key unless self.has_key? key
     super
   end
 
-  def clear
+  def clear # :nodoc:
     super
     @prefix.clear
     @order.clear
     @attribs.clear
     @edge.clear
   end
+
+  ##
+  # Returns a Hash with a count of the outgoing edges for each node.
 
   def counts
     result = Hash.new 0
@@ -29,10 +62,20 @@ class Graph < Hash
     result
   end
 
-  def delete key
+  def delete key # :nodoc:
     @order.delete key
+    # TODO: prolly needs to go clean up attribs
     super
   end
+
+  ##
+  # Overrides Hash#each_pair to go over each _edge_. Eg:
+  #
+  #   g["a"] << "b"
+  #   g["a"] << "b"
+  #   g.each_pair { |from, to| ... }
+  #
+  # goes over a -> b *twice*.
 
   def each_pair
     @order.each do |from|
@@ -42,6 +85,9 @@ class Graph < Hash
     end
   end
 
+  ##
+  # Deletes nodes that have less than minimum number of outgoing edges.
+
   def filter_size minimum
     counts.each do |node, count|
       next unless count < minimum
@@ -49,13 +95,16 @@ class Graph < Hash
     end
   end
 
-  def initialize
+  def initialize # :nodoc:
     super { |h,k| h[k] = [] }
     @prefix  = []
     @order   = []
     @attribs = Hash.new { |h,k| h[k] = [] }
     @edge    = Hash.new { |h,k| h[k] = Hash.new { |h2,k2| h2[k2] = [] } }
   end
+
+  ##
+  # Creates a new Graph whose edges point the other direction.
 
   def invert
     result = self.class.new
@@ -64,6 +113,9 @@ class Graph < Hash
     end
     result
   end
+
+  ##
+  # Returns a list of keys sorted by number of outgoing edges.
 
   def keys_by_count
     counts.sort_by { |key, count| -count }.map {|key, count| key }
@@ -92,12 +144,19 @@ class Graph < Hash
     end
   end
 
+  ##
+  # Saves out both a dot file to path and an image for the specified type.
+  # Specify type as nil to skip exporting an image.
+
   def save path, type="png"
     File.open "#{path}.dot", "w" do |f|
       f.write self.to_s
     end
     system "dot -T#{type} #{path}.dot > #{path}.#{type}" if type
   end
+
+  ##
+  # Outputs a graphviz graph.
 
   def to_s
     result = []
