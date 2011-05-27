0a57da52ff4850987aa029ddd1671ef5445b868e
Ryan Davis <ryand@zenspider.com>
2011-05-26 17:34:55 -0800

    prepping for release [git-p4: depot-paths = "//src/graph/dev/": change = 6263]

--- lib/graph.rb
+++ lib/graph.rb
@@ -7,7 +7,7 @@ require "enumerator"
 # dot format.
 
 class Graph
-  VERSION = "2.0.1" # :nodoc:
+  VERSION = "2.1.0" # :nodoc:
 
   LIGHT_COLORS = %w(gray lightblue lightcyan lightgray lightpink
                     lightslategray lightsteelblue white)
